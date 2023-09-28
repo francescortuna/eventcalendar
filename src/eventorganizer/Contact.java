aceaf956a08b2055c471bc001745122ebb3af8a7
Frances Cortuna <francescortuna@gmail.com>
2023-09-28 19:31:55 -0400

    Added comments

--- src/eventorganizer/Contact.java
+++ src/eventorganizer/Contact.java
@@ -25,18 +25,18 @@ public class Contact {
      * @return boolean
      */
     public boolean isValid() {
-        StringTokenizer tokenizer = new StringTokenizer(email, "@");
-        while(tokenizer.hasMoreTokens()) {
+        StringTokenizer tokenizer = new StringTokenizer(email, "@"); // Creates new StringTokenizer object to separate email into tokens
+        while(tokenizer.hasMoreTokens()) { // Iterates through tokens
             String token = tokenizer.nextToken();
-            if(token.equals("rutgers.edu")) {
-                for(Department departmentEnum : Department.values()) {
+            if(token.equals("rutgers.edu")) { // Checks if a token equals rutgers.edu domain
+                for(Department departmentEnum : Department.values()) { // If email is valid, then checks if department name is valid by iterating through Department values
                     String contactDepartment = department.getFullName();
-                    if(contactDepartment.equals(departmentEnum.getFullName())) {
-                        return true;
+                    if(contactDepartment.equals(departmentEnum.getFullName())) { 
+                        return true; // If department name is also valid then return true as both fields are valid
                     }
                 }
             }
         }
-        return false;
+        return false; // If at least one field is incorrect, return false
     }
 }
